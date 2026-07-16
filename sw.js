a455fbcd3ef427fb07093c8c7fa2af52c33f40cd
stefanhasselm74314-byte <stefanhasselm.74314@gmail.com>
2026-07-16 16:05:49 +0200

    Refresh offline cache for MVP 0.5

--- sw.js
+++ sw.js
@@ -1,23 +1,38 @@
 'use strict';
 
-const CACHE_NAME = 'universelab-mvp-0.3.0';
-const APP_SHELL = ['./','./index.html','./manifest.webmanifest'];
+const CACHE_NAME = 'universelab-mvp-0.5.0';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './src/numerics.js'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    if (response && response.status === 200 && response.type !== 'opaque') {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-    }
-    return response;
-  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html'))));
+  event.respondWith(
+    fetch(event.request).then(response => {
+      if (response && response.status === 200 && response.type !== 'opaque') {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
 });
